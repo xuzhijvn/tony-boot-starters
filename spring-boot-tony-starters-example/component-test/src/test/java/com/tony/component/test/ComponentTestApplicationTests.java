7a13fbf3a36ce66deab1e79ecf52a714ecbca61e
徐植君 <tony121.xu@huolala.cn>
2021-08-19 18:09:56 +0800

    component优化

---
+++ spring-boot-tony-starters-example/component-test/src/test/java/com/tony/component/test/ComponentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.component.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
