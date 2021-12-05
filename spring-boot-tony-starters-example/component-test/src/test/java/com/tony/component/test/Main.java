34919e11c20d40ad40fc3ea62cecd91638a48fa9
徐植君 <zhaowoganmane@gmail.com>
2021-12-05 13:38:36 +0800

    readme

---
+++ spring-boot-tony-starters-example/component-test/src/test/java/com/tony/component/test/Main.java
@@ -0,0 +1,50 @@
+/*
+ *       Copyright© (2020).
+ */
+package com.tony.component.test;
+
+import cn.hutool.core.util.NumberUtil;
+
+/**
+ * @author tony
+ * @create 2021-12-01
+ * @description:
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static long format(String str) {
+        if (NumberUtil.isNumber(str)) {
+            return Long.parseLong(str);
+        }
+        if (str.charAt(str.length() - 1) != 'K' ||
+                str.charAt(str.length() - 1) != 'M' ||
+                str.charAt(str.length() - 1) != 'G'
+        ) {
+            return -1;
+        }
+        String numStr = str.substring(0, str.length() - 1);
+        if (!NumberUtil.isNumber(numStr)) {
+            return -1;
+        }
+        long num = Integer.parseInt(numStr);
+        if (str.charAt(str.length() - 1) == 'G' && num > 4) {
+            return -1;
+        }
+
+        if (str.charAt(str.length() - 1)  == 'K'){
+            return num * 1024;
+        }
+        if (str.charAt(str.length() - 1)  == 'M'){
+            return num * 1024 * 1024;
+        }
+        if (str.charAt(str.length() - 1)  == 'G'){
+            return num * 1024 * 1024 * 1024;
+        }
+        return -1;
+    }
+
+}
